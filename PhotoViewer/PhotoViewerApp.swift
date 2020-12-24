56bb9e8f206cce40286565a92e7fe99e378f4c95
Alisher <Alisher>
2020-12-24 14:15:56 +0600

    init xcode project

---
+++ PhotoViewer/PhotoViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoViewerApp.swift
+//  PhotoViewer
+//
+//  Created by Алишер Алсейт on 24.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct PhotoViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
